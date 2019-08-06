184b4aeb7168ebe7742eeac8548af1308cc6f521
shubh <psubham94@gmail.com>
2019-08-06 15:51:28 +0530

    init commit

---
+++ server.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var fs = require('fs');
+var app = express();
+
+
+const server = require('http').createServer(app);
+
+const io = require('socket.io')(server);
+
+var lines = 10;
+
+io.on('connection', (socket) => {
+  var newdata = readFileContent();
+  socket.emit('changed', newdata);
+
+  socket.on('lines', (line) => {
+    lines = line || 10;
+    var newdata = readFileContent();
+    socket.emit('changed', newdata);
+  })
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+fs.watchFile('text.txt',{persistent:true, interval:1000}, (data) => { 
+  var newdata = readFileContent();
+  io.sockets.emit('changed', newdata);
+});
+
+
+
+function readFileContent() {
+  var fileData = fs.readFileSync('text.txt', 'utf8');
+  fileData = fileData.trim().split('\n');
+  return fileData.length > lines ?
+    fileData.slice(fileData.length - lines)
+  :
+    fileData
+}
+
+server.listen(3000, () => {
+  console.log('listening on port 3000');
+})
